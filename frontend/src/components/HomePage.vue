4a0ec0f3462d31ea7e300a54aa58f33b90fe431d
Aka-sh11 <akasharma.py@gmail.com>
2024-03-02 18:54:20 +0530

    refactored frontend folder

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <body>
+        <div class="wrapper">
+            <nav>
+                <router-link to="/" exact>
+                    <img src="../assets/logo.png" alt="Jiggly Logo" class="logo" />
+                </router-link>
+                <router-link to="/admin">Admin</router-link>
+            </nav>
+    </div>
+    <div class="container">
+        <h1> Jiggly : Your Music Companion</h1>
+        <p> Jiggly is a music player that allows you to listen to your favorite music on the go. </p>
+
+        <button type="button" class="btn btn-primary btn-lg m-5">Signup</button>
+        <button type="button" class="btn btn-secondary btn-lg m-5">Login</button>
+    </div>
+    </body>
+    </template>
+
+<style scoped>
+</style>
