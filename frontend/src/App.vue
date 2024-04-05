9949c2305f136cd09f581573b6321003787601cc
Aka-sh11 <akasharma.py@gmail.com>
2024-04-05 23:39:26 +0530

    authorisation done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { useAuthStore } from './stores/authStore';
+
+const store = useAuthStore();
+
+store.fetchUserInfo();
 </script>
 
 <template>
